99e2a99934259f400fcb1cc074ed8b052866517d
skydogzz <thomas.stephan@live.fr>
2023-07-05 08:46:50 +0000

    create a json with only the needed content

--- data/app.py
+++ data/app.py
@@ -8,6 +8,7 @@ load_dotenv('.env')
 # Récupérer les valeurs des variables d'environnement
 zendeskExportFilename = os.getenv('ZENDESK_EXPORT_FILENAME')
 zendeskExportFilenameRepaired = os.getenv('ZENDESK_EXPORT_FILENAME_REPAIRED')
+search = os.getenv('SEARCH')
 
 # Ouvrir le fichier contenant la liste de JSON
 with open(zendeskExportFilename) as f:
@@ -19,11 +20,22 @@ formatted_data = '[' + ','.join(lines).rstrip('\n') + ']'
 # Analyser le tableau JSON en une structure de données Python
 data = json.loads(formatted_data)
 
-# Parcourir les éléments du tableau JSON
-for element in data:
-    # Faire quelque chose avec chaque élément
-    print(element)
+# Liste pour stocker les éléments sans la chaîne recherchée
+filtered_data = []
 
-# Écrire le tableau JSON dans un fichier
+# Parcourir les éléments du JSON
+for element in data:
+    # Convertir l'élément en chaîne de caractères
+    element_str = json.dumps(element)
+    
+    # Vérifier si la chaîne recherchée est présente dans l'élément
+    if search in element_str:
+        # L'élément contient la chaîne recherchée, ne rien faire
+        pass
+    else:
+        # L'élément ne contient pas la chaîne recherchée, l'ajouter à la liste filtrée
+        filtered_data.append(element)
+
+# Écrire le JSON filtré dans un fichier
 with open(zendeskExportFilenameRepaired, 'w') as f:
-    json.dump(data, f)
+    json.dump(filtered_data, f)
